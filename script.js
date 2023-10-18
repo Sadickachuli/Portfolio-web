feeee08124e2ff033492ae9847fce1923a14cdf3
Sadickachuli <m.achuli@alustudent.com>
2023-10-18 23:32:05 +0200

    comm

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll('.skills img, .experience img, .education img, .projects img');
+    const textDivs = document.querySelectorAll('.text-box');
+    const scaleFactor = 1.28; // This controls the zoom level
+    const isImageVisible = Array(images.length).fill(true);
+  
+    images.forEach((image, index) => {
+      image.addEventListener('mouseenter', () => {
+        if (isImageVisible[index]) {
+          image.style.borderRadius = '50%'; // Makes the image circular when the mouse hovers over it
+          textDivs[index].style.display = 'none'; // Hides the text-box
+          image.style.transform = `scale(${scaleFactor})`; // This will zoom in the image
+          image.style.transition = 'transform 0.3s'; // To add a smooth transition effect
+        }
+      });
+  
+      image.addEventListener('mouseleave', () => {
+        if (isImageVisible[index]) {
+          image.style.borderRadius = '50px'; // Revert to the original shape
+          textDivs[index].style.display = 'none'; // Hide the text-box
+          image.style.transform = 'scale(1)'; // Revert to the original size
+        }
+      });
+  
+      image.addEventListener('click', () => {
+        isImageVisible[index] = !isImageVisible[index]; // Toggle image visibility
+        image.style.display = isImageVisible[index] ? 'block' : 'none'; // Hide or show the image
+        textDivs[index].style.display = isImageVisible[index] ? 'none' : 'block'; // Show or hide the text-box
+      });
+  
+      textDivs[index].addEventListener('click', () => {
+        isImageVisible[index] = !isImageVisible[index]; // Toggle image visibility when text is clicked
+        image.style.display = isImageVisible[index] ? 'block' : 'none'; // Hide or show the image
+        textDivs[index].style.display = isImageVisible[index] ? 'none' : 'block'; // Show or hide the text-box
+      });
+    });
+  });
+  
